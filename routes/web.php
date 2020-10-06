403774d3b0d36df3875d39cb8e78559ff88ba5c8
Khai Rahman <khairahmanplus@gmail.com>
2020-10-06 11:11:10 +0800

    Tambah route daftar pengguna

--- routes/web.php
+++ routes/web.php
@@ -6,5 +6,8 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+// Pendaftaran pengguna
 Route::get('daftar', 'DaftarController@paparBorangDaftar')->name('daftar.papar');
+Route::post('daftar', 'DaftarController@daftar')->name('daftar');
+// Log masuk pengguna
 Route::get('log-masuk', 'LogMasukController@paparBorangLogMasuk')->name('log-masuk.papar');
